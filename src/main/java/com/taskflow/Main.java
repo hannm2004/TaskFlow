8fdabf7325abc11e135ba6293bde279c3e4c63fd
hannm2004 <hanminh2709@gmail.com>
2026-09-13 11:35:35 +0700

    feat: add task collection

--- src/main/java/com/taskflow/Main.java
+++ src/main/java/com/taskflow/Main.java
@@ -1,12 +1,18 @@
 package com.taskflow;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("================================");
         System.out.println("        TASKFLOW PROJECT");
         System.out.println("================================");
         System.out.println("Hello, TaskFlow!");
+
+        List<Task> tasks = new ArrayList<>();
 
         tasks.add(new Task(
                 "Implement Login",
@@ -32,29 +38,18 @@ public class Main {
                 0
         ));
 
-        //String status = TaskUtils.getTaskStatus(progress);
+        System.out.println("\nTask List:");
 
-        System.out.println("Task: " + task.getTitle());
-        System.out.println("Priority: " + task.getPriority());
-        System.out.println("Progress: " + task.getProgress() + "%");
-        System.out.println("Completed: " + completed);
-        System.out.println("Status: " + task.getStatus());
-
-        String[] tasks = {
-                "Implement Login",
-                "Design Database",
-                "Create REST API",
-                "Implement Docker",
-                "Write Tests"
-        };
-        for (int i = 0; i < tasks.length; i++){
-            System.out.println("Task " + (i+1) + ": " + tasks[i]);
-        }
-
-        int[] progresses = {100, 70, 40, 30, 0};
-        for (int i = 0; i < progresses.length; i++)
-        {
-            System.out.println(tasks[i] + " - " + TaskUtils.getTaskStatus(progresses[i]));
+        for (Task task : tasks) {
+            System.out.println(
+                    task.getTitle()
+                            + " | "
+                            + task.getPriority()
+                            + " | "
+                            + task.getProgress()
+                            + "% | "
+                            + task.getStatus()
+            );
         }
     }
 }
